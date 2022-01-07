0fba1438f9843e3265da0c7296a50889a40592dc
Cristian Tabacitu <cristitabacitu@gmail.com>
2022-01-07 10:51:35 +0200

    add optional details and link to 500 error page

--- src/resources/error_views/500.blade.php
+++ src/resources/error_views/500.blade.php
@@ -2,15 +2,39 @@
 
 @php
 	$error_number = 500;
+	$title = "It's not you, it's me.";
+	$description = "An internal server error has occurred. If the error persists please contact the development team.";
+
+	if (isset($exception)) {
+		// if a message exists in the error, show that
+		if ($exception->getMessage()) {
+			$description = $exception->getMessage();
+		}
+		// if title and description have been given as headers, show those
+		$title = $exception->getHeaders()['title'] ?? $title;
+		$description = $exception->getHeaders()['description'] ?? $description;
+		$details = $exception->getHeaders()['details'] ?? false;
+		$link = $exception->getHeaders()['link'] ?? false;
+	}
 @endphp
 
 @section('title')
-	It's not you, it's me.
+	{!! $title !!}
 @endsection
 
 @section('description')
-	@php
-	  $default_error_message = "An internal server error has occurred. If the error persists please contact the development team.";
-	@endphp
-	{!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
+	<span class="text-danger">{!! $description !!}</span>
+
+	@if ($details || $link)
+	<div class="col-md-10 offset-1">
+		@if ($details)
+			<p class="mt-5"><small>{!! $details !!}</small></p>
+		@endif
+
+		@if ($link)
+			<a target="_blank" href="{{ $link }}" class="btn btn-default"><i class="la la-external-link-alt"></i> Read more</a>
+		@endif
+	</div>
+	@endif
+
 @endsection
