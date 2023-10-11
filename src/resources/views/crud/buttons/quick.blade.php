b5e4e40c77c7d5202ce523da26dbe62d41f94187
Pedro X. <pxpm88@gmail.com>
2023-10-11 16:28:41 +0100

    add span to backpack button text

--- src/resources/views/crud/buttons/quick.blade.php
+++ src/resources/views/crud/buttons/quick.blade.php
@@ -29,6 +29,6 @@
         @endforeach
         >
         @if ($icon) <i class="{{ $icon }}"></i> @endif
-        {{ $label }}
+        <span class="{{$button->name}}-button-text-span">{{ $label }}</span>
     </{{ $wrapper['element'] }}>
 @endif
